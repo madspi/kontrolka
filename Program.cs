384b143bad04a85392ba5ed46170562f2044d63a
madspi <madspibs@gmail.com>
2024-07-31 11:41:01 +0300

    napisal programmu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class HomeWork
+{
+    public static int Main()
+    {
+        var input = Console.ReadLine().Split(',');
+        PrintLess4(input);
+        return 0;
+    }
+
+    private static void PrintLess4(string[] input)
+    {
+        var count = input.Length - 1;
+        for (int i  = 0; i < count; i++)
+        {
+            if (input[i].Length < 4)
+            {
+                Console.Write(input[i] + ",");
+            }
+        }
+        if (count >= 0 && input[count].Length < 4)
+        {
+            Console.Write(input[count]);
+        }
+    }
+}
